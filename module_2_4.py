51146dffa7cd2852ba43171af863468b321336f6
pilgrim-nord <dmelekhov@gmail.com>
2024-12-14 19:05:43 +0300

    Домашнее задание 4 по модулю 2 c использованием переменной для проверки истинности

--- module_2_4.py
+++ module_2_4.py
@@ -1,35 +1,15 @@
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-# primes, not_primes = [], []
-# for i in numbers:
-#     if i == 1:
-#         not_primes.append(i)
-#         continue
-#     for j in numbers[1 :]:
-#         if i % j != 0:
-#             continue
-#         if i % j == 0 and i == j:
-#             primes.append(i)
-#             break
-#         else:
-#             if i % j == 0 and i != j:
-#                 not_primes.append(i)
-#             break
-#
-#
-# print('Primes:', primes)
-# print('Not primes:', not_primes)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for num in numbers:
+for i in numbers[1 :]:
     is_prime = True
-    for i in range(2, num):
-        if num % i == 0:
+    for j in range(2, i):
+        if i % j == 0:
             is_prime = False
             break
     if is_prime:
-        primes.append(num)
+        primes.append(i)
     else:
-        not_primes.append(num)
+        not_primes.append(i)
 print('Primes:', primes)
 print('Not primes:', not_primes)
