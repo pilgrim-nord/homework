de55426d37cb1489fa0c448151eedeb6d1de2299
pilgrim-nord <dmelekhov@gmail.com>
2024-12-14 17:02:30 +0300

    Домашнее задание 4 по модулю 2

--- module_2_4.py
+++ module_2_4.py
@@ -1,2 +1,35 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# primes, not_primes = [], []
+# for i in numbers:
+#     if i == 1:
+#         not_primes.append(i)
+#         continue
+#     for j in numbers[1 :]:
+#         if i % j != 0:
+#             continue
+#         if i % j == 0 and i == j:
+#             primes.append(i)
+#             break
+#         else:
+#             if i % j == 0 and i != j:
+#                 not_primes.append(i)
+#             break
+#
+#
+# print('Primes:', primes)
+# print('Not primes:', not_primes)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes, not_primes = [], []
+primes = []
+not_primes = []
+for num in numbers:
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
+print('Primes:', primes)
+print('Not primes:', not_primes)
