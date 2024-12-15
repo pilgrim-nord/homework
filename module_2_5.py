ac9cd10457dada64b2236a150c5558ebb6d7be1d
pilgrim-nord <dmelekhov@gmail.com>
2024-12-15 14:12:00 +0300

    Домашнее задание 5 по модулю 2 доделано

--- module_2_5.py
+++ module_2_5.py
@@ -1,1 +1,15 @@
 def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        if m <= 0:
+            break
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+result1 = get_matrix(5, 3, 6)
+result2 = get_matrix(0, 5, 6)
+result3 = get_matrix(5, -25, 6)
+print(result1)
+print(result2)
+print(result3)
