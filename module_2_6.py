f06ac9604bc6d53854bb923855ce17f7911ea264
pilgrim-nord <dmelekhov@gmail.com>
2024-12-16 14:01:03 +0300

    Доп. задание по модулю 2

---
+++ module_2_6.py
@@ -0,0 +1,8 @@
+n = int(input())
+list_ = []
+for i in range(1, n // 2 + 1):
+    for j in range(1, n  + 1):
+        if n % (i + j) == 0 and i < j:
+            list_.append(i)
+            list_.append(j)
+print (*list_, sep='')
