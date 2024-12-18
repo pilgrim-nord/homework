b341c15313d125e33591380f37ed4bd52f9d398d
pilgrim-nord <dmelekhov@gmail.com>
2024-12-18 10:32:22 +0300

    Задание по пространству имен

---
+++ module_3_1.py
@@ -0,0 +1,31 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    a = (len(string), string.upper(), string.lower())
+    return a
+
+def is_contains(string, listik):
+    count_calls()
+    is_con = False
+    for i in range(len(listik)):
+        temp = listik[i]
+        if temp.lower() == string.lower():
+            is_con = True
+            break
+    return is_con
+
+
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
+print(calls)
