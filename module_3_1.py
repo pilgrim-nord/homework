286608baed6dfb08aeecf91cebb5c8e90dd26f80
pilgrim-nord <dmelekhov@gmail.com>
2024-12-18 10:33:52 +0300

    Задание по пространству имен(исправленное)

--- module_3_1.py
+++ module_3_1.py
@@ -24,8 +24,8 @@ print(string_info('Capybara'))
 
 print(string_info('Armageddon'))
 
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
 
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('cycle', ['recycling', 'cyclic']))
 
 print(calls)
