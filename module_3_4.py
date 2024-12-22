bd0316ebc831650aa2b877be85d49d0b39f3ed94
pilgrim-nord <dmelekhov@gmail.com>
2024-12-22 20:57:27 +0300

    Задача "Однокоренные" 1

---
+++ module_3_4.py
@@ -0,0 +1,11 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for other_word in other_words:
+        if other_word.lower() in root_word.lower() or root_word.lower() in other_word.lower():
+            same_words.append(other_word)
+    return(same_words)
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
