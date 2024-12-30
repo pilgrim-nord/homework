c36b32cf8fa9dbe6ff75b5538c04bffc491a059b
pilgrim-nord <dmelekhov@gmail.com>
2024-12-30 12:07:43 +0300

    Дополнительное практическое задание по модулю: "Подробнее о функциях."

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(x, y):
+    if y == 0:
+        return 'Ошибка'
+    else:
+        return x / y
---
+++ true_math.py
@@ -0,0 +1,6 @@
+import math
+def divide(x, y):
+    if y == 0:
+        return math.inf
+    else:
+        return x / y
